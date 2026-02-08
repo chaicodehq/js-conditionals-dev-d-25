b9292ad6ab38f453819c33599037302960cb16e7
DEV <175703830+dev-d-25@users.noreply.github.com>
2026-02-08 16:50:13 +0530

    assigment 3 done

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -26,4 +26,31 @@
  */
 export function calculateGrade(score, hasExtraCredit) {
   // Your code here
+  if(score <0 || score > 100) return "INVALID";
+  if(hasExtraCredit) {
+    score = score + 5
+    if(score > 100){
+      score = 100
+    }
+  }
+
+  let grade;
+
+  if(score <=59) {
+    grade = "F"
+  }
+  else if (score <= 69){
+    grade = "D"
+  }
+  else if (score <= 79){
+    grade = "C"
+  }
+  else if (score <= 89){
+    grade = "B"
+  }
+  else if (score <= 100){
+    grade = "A"
+  }
+  return grade
 }
+
